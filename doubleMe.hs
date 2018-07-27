7f0569a2868c813902d2bb6084781a1b36cf76d3
rkuma199 <ranjith.c.kumar@capgemini.com>
2018-07-27 18:22:18 +0530

    Exercise file added

--- doubleMe.hs
+++ doubleMe.hs
@@ -6,3 +6,15 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber' x = (if x > 100
                           then x
                             else x*2 ) + 1
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+charName :: char -> String
+
+charName 'a' = "Apple"
+charName 'b' = "Ball"
