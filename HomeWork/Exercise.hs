f8ba0d478482b0884de50e475dc69fe013b559d5
rkuma199 <ranjith.c.kumar@capgemini.com>
2018-07-30 18:34:41 +0530

    Digit to list conversion added

---
+++ HomeWork/Exercise.hs
@@ -0,0 +1,27 @@
+-- Exercise 1 We need to first find the digits of a number. Define the
+-- functions
+ -- toDigits    :: Integer -> [Integer]
+ -- toDigitsRev :: Integer -> [Integer]
+-- toDigits should convert positive Integers to a list of digits. (For 0 or negative inputs, toDigits should return the empty list.) toDigitsRev should do the same, but with the digits reversed.
+-- Example: toDigits 1234 == [1,2,3,4] Example: toDigitsRev 1234 == [4,3,2,1] Example: toDigits 0 == []
+-- Example: toDigits (-17) == []
+
+-- toDigits :: Integer -> [Integer]
+-- toDigitsRev :: Integer -> [Integer]
+
+-- toDigits n = n `mod` 10
+--       | if n < 0
+--       | 0 else n
+
+sumtorial :: Integer -> Integer
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n-1)
+
+-- toDigits :: Integer -> Integer
+-- toDigits 0 = 0
+-- toDigits n = n `mod` 10 + toDigits(n-1)
+
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+length' xs = sum[ 1 | _ <- xs]
+
+toDigits n = [read [c] :: Integer | c <- show n]
