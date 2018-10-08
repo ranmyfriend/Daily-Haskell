08cf16de54677a87a46dfd29c190bf75eb41221c
rkuma199 <ranjith.c.kumar@capgemini.com>
2018-10-08 16:03:44 +0530

    Added Multiple params functions

---
+++ Simple_Functions_MoreThan1Arg.hs
@@ -0,0 +1,12 @@
+{-
+    Language: Haskell
+    Topic: Simple Functions to compute with more than one params
+    Creator: Ranjith
+    Date: October 8th, 2018
+-}
+
+average :: Float -> Float -> Float
+average x y = (x + y)/2
+
+addMultiply :: Int -> Int -> Int -> Int
+addMultiply x y z = (x + y) * z
