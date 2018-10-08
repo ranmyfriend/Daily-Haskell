110cf65c0a4b9a563f21580a79d8870b06231213
rkuma199 <ranjith.c.kumar@capgemini.com>
2018-10-08 16:29:30 +0530

    Excercise are completed on Basic fundamentals

--- Simple_Functions_MoreThan1Arg.hs
+++ Simple_Functions_MoreThan1Arg.hs
@@ -1,3 +1,4 @@
+import Prelude
 {-
     Language: Haskell
     Topic: Simple Functions to compute with more than one params
@@ -10,3 +11,10 @@ average x y = (x + y)/2
 
 addMultiply :: Int -> Int -> Int -> Int
 addMultiply x y z = (x + y) * z
+
+showResult :: Int -> String
+showResult x = "Hello" ++ " " ++ show x
+
+--Area of Circle: Pi*r^2
+areaOfCircle :: Float -> String
+areaOfCircle r = "The area of a circle with radius " ++ show r ++ "cm is about " ++ show (pi * r * r) ++ "cm^2"
